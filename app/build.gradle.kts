d2dbbc8c4f3b20d1506d29642d1d0004c40bf1e7
jtorrast <jordi.torras89@gmail.com>
2023-10-12 01:08:49 +0200

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.banco_jototo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.banco_jototo"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
