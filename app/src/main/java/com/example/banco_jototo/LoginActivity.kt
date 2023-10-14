aa56cce711a7ce190d4bd46e703f30ba5d895baf
jtorrast <jordi.torras89@gmail.com>
2023-10-14 02:17:32 +0200

    Interface login terminada, cambio pantalla conseguido, faltan comprobaciones

--- app/src/main/java/com/example/banco_jototo/LoginActivity.kt
+++ app/src/main/java/com/example/banco_jototo/LoginActivity.kt
@@ -1,11 +1,27 @@
 package com.example.banco_jototo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.banco_jototo.databinding.ActivityLoginBinding
+import com.example.banco_jototo.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        //setContentView(R.layout.activity_login)
     }
 }
