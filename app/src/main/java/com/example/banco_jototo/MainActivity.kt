b3434ac07351c5bfda523fba2df23498afd6db05
jtorrast <jordi.torras89@gmail.com>
2023-10-14 06:47:40 +0200

    Inicio vista bienvenida y paso de DNI consguido

--- app/src/main/java/com/example/banco_jototo/MainActivity.kt
+++ app/src/main/java/com/example/banco_jototo/MainActivity.kt
@@ -2,14 +2,22 @@ package com.example.banco_jototo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.banco_jototo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val dni = intent.getStringExtra("dni")
+
+        binding.viewDni.text = dni
     }
 }
