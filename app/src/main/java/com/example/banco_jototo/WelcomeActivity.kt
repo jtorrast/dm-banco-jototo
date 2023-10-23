51406a56ca0d3b8a2dee5b737808beb358294a99
jtorrast <jordi.torras89@gmail.com>
2023-10-23 11:19:16 +0200

    Logica botones WelcomeActivity implementada

--- app/src/main/java/com/example/banco_jototo/WelcomeActivity.kt
+++ app/src/main/java/com/example/banco_jototo/WelcomeActivity.kt
@@ -1,11 +1,27 @@
 package com.example.banco_jototo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.banco_jototo.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //boton continuar
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        //boton salir aplicación
+        binding.btnExit.setOnClickListener {
+            finish()
+        }
     }
 }
