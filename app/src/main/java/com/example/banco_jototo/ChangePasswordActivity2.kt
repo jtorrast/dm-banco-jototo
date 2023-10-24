a00712fe31cd9bcba6a58299fd9f4da5bb44d7f9
jtorrast <jordi.torras89@gmail.com>
2023-10-24 15:42:43 +0200

    Text edit cambiar contraseña

---
+++ app/src/main/java/com/example/banco_jototo/ChangePasswordActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.banco_jototo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangePasswordActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password2)
+    }
+}
