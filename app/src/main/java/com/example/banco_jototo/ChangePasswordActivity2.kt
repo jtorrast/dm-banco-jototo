1a1e724552e333405d2966e8cb1b5a0eb72b0a16
jtorrast <jordi.torras89@gmail.com>
2023-10-25 05:06:54 +0200

    Comprobaciones fields cambio contraseña con onFocusListenner

--- app/src/main/java/com/example/banco_jototo/ChangePasswordActivity2.kt
+++ app/src/main/java/com/example/banco_jototo/ChangePasswordActivity2.kt
@@ -2,6 +2,7 @@ package com.example.banco_jototo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.banco_jototo.databinding.ActivityChangePassword2Binding
 import com.google.android.material.snackbar.Snackbar
 
@@ -14,13 +15,79 @@ class ChangePasswordActivity2 : AppCompatActivity() {
         binding = ActivityChangePassword2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.newPasswordEditFieldText.onFocusChangeListener =View.OnFocusChangeListener { v, hasFocus ->
+            var password = binding.newPasswordEditFieldText.text.toString()
+            if (!hasFocus){
+                if (password.isEmpty()){
+                    binding.newPasswordFieldTex.error = getString(R.string.error_empty_edit_text)
+                }else{
+                    binding.newPasswordFieldTex.error = null
+
+                    if (password.length < 5){
+                        binding.newPasswordFieldTex.error = getString(R.string.error_password_length)
+                    }else{
+                        binding.newPasswordFieldTex.error = null
+                    }
+
+                }
+
+            }
+        }
+
+        binding.confirmPasswordFieldEditTex.onFocusChangeListener =View.OnFocusChangeListener { v, hasFocus ->
+            var password = binding.confirmPasswordFieldEditTex.text.toString()
+            if (!hasFocus){
+                if (password.isEmpty()){
+                    binding.confirmPasswordFieldTex.error = getString(R.string.error_empty_edit_text)
+                }else{
+                    binding.confirmPasswordFieldTex.error = null
+
+                    if (password.length < 5){
+                        binding.confirmPasswordFieldTex.error = getString(R.string.error_password_length)
+                    }else{
+                        binding.confirmPasswordFieldTex.error = null
+                    }
+                }
+
+
+            }
+
+
+
+        }
+
         binding.btnConfirmPass.setOnClickListener {
             var newPass = binding.newPasswordEditFieldText.text.toString()
             var confirmNewPass = binding.confirmPasswordFieldEditTex.text.toString()
 
+            if (newPass.isEmpty()){
+                binding.newPasswordFieldTex.error = getString(R.string.error_empty_edit_text)
+                return@setOnClickListener
+            }
+
+            if (confirmNewPass.isEmpty()){
+                binding.confirmPasswordFieldTex.error = getString(R.string.error_empty_edit_text)
+                return@setOnClickListener
+            }
+
+            if (newPass.length < 5){
+                binding.newPasswordFieldTex.error = getString(R.string.error_password_length)
+                return@setOnClickListener
+            }else{
+                binding.newPasswordFieldTex.error = null
+            }
+
+            if (confirmNewPass.length < 5){
+                binding.confirmPasswordFieldTex.error = getString(R.string.error_password_length)
+                return@setOnClickListener
+            }else{
+                binding.confirmPasswordFieldTex.error = null
+            }
+
 
             if (newPass.equals(confirmNewPass)){
-
+                binding.newPasswordEditFieldText.text = null
+                binding.confirmPasswordFieldEditTex.text = null
                 Snackbar.make(binding.root,getString(R.string.snackbar_pass_change), Snackbar.LENGTH_SHORT).setAnchorView(binding.guideline).show()
 
 
@@ -30,7 +97,6 @@ class ChangePasswordActivity2 : AppCompatActivity() {
 
         }
 
-        binding.confirmPasswordFieldEditTex.onFocusChangeListener
 
         binding.btnExit.setOnClickListener {
             finish()
