b591df7f2dbb3719683c35fe45b047b6a00780fc
jtorrast <jordi.torras89@gmail.com>
2023-11-01 12:59:03 +0100

    Estilo y layout de transferencuias terminado

--- app/src/main/java/com/example/banco_jototo/TransferActivity.kt
+++ app/src/main/java/com/example/banco_jototo/TransferActivity.kt
@@ -3,9 +3,7 @@ package com.example.banco_jototo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import androidx.core.graphics.toColor
 import com.example.banco_jototo.databinding.ActivityTransferBinding
 
 class TransferActivity : AppCompatActivity() {
@@ -20,27 +18,27 @@ class TransferActivity : AppCompatActivity() {
 
         //emisor
         val datos = arrayOf("ES71-4857-6875-1234", "ES71-4857-6452-6423")
-        val adapterOwnAccount = ArrayAdapter(this, R.layout.spinner_currency_item, datos)
+        val adapterOwnAccount = ArrayAdapter(this, R.layout.spinner_item, datos)
 
         val spinnerOwnAccount = binding.spinnerAccount
-        adapterOwnAccount.setDropDownViewResource(R.layout.spiner_drop_currency_item)
+        adapterOwnAccount.setDropDownViewResource(R.layout.spiner_drop_item)
         spinnerOwnAccount.adapter = adapterOwnAccount
 
         //receptor
-        val adapterReciberOwnAccount = ArrayAdapter(this, R.layout.spinner_currency_item, datos)
+        val adapterReciberOwnAccount = ArrayAdapter(this, R.layout.spinner_item, datos)
 
         val spinnerReciberOwnAccount = binding.spinnerReceiverOwnAccount
-        adapterReciberOwnAccount.setDropDownViewResource(R.layout.spiner_drop_currency_item)
+        adapterReciberOwnAccount.setDropDownViewResource(R.layout.spiner_drop_item)
         spinnerReciberOwnAccount.adapter = adapterReciberOwnAccount
 
 
         //datos spinner simbolo divisa
         val divisas = arrayOf("€", "$", "GBP")
-        val adapterCurrency = ArrayAdapter(this, R.layout.spinner_currency_item, divisas)
+        val adapterCurrency = ArrayAdapter(this, R.layout.spinner_item, divisas)
 
         val spinnerCurrency = binding.spCurrency
 
-        adapterCurrency.setDropDownViewResource(R.layout.spiner_drop_currency_item)
+        adapterCurrency.setDropDownViewResource(R.layout.spiner_drop_item)
 
         spinnerCurrency.adapter = adapterCurrency
 
