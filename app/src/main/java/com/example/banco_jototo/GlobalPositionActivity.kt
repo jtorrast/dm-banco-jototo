4eefb7b44589b68b3db290b1b92400a1515e85f1
jtorrast <jordi.torras89@gmail.com>
2023-11-06 11:57:57 +0100

    Activity global position added

---
+++ app/src/main/java/com/example/banco_jototo/GlobalPositionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.banco_jototo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GlobalPositionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_global_position)
+    }
+}
