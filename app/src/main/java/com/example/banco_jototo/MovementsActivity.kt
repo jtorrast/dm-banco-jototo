3513e7f970ddbb6c5aa7f4659eff76c0ace07d72
jtorrast <jordi.torras89@gmail.com>
2023-11-06 15:19:36 +0100

    MovementsActivity iniciada

---
+++ app/src/main/java/com/example/banco_jototo/MovementsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.banco_jototo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovementsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movements)
+    }
+}
