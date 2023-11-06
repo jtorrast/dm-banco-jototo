d582b3ae4d7c2a3a6322fb3c979e596a9456bdc6
jtorrast <jordi.torras89@gmail.com>
2023-11-06 17:21:05 +0100

    Movements funciona, falta definir estilo de cada item, se muestra con estilo de los items posición global

--- app/src/main/java/com/example/banco_jototo/MovementsActivity.kt
+++ app/src/main/java/com/example/banco_jototo/MovementsActivity.kt
@@ -2,14 +2,22 @@ package com.example.banco_jototo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.Spinner
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.banco_jototo.bd.MiBancoOperacional
 import com.example.banco_jototo.databinding.ActivityMovementsBinding
 import com.example.banco_jototo.pojo.Cliente
 import com.example.banco_jototo.pojo.Cuenta
+import com.example.banco_jototo.pojo.Movimiento
+import com.google.android.material.snackbar.Snackbar
 
-class MovementsActivity : AppCompatActivity() {
-
+class MovementsActivity : AppCompatActivity(){
+    private lateinit var movementsAdapter: MovementsAdapter
+    private lateinit var linearLayoutManager: LinearLayoutManager
     private lateinit var binding: ActivityMovementsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +25,7 @@ class MovementsActivity : AppCompatActivity() {
         binding = ActivityMovementsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //generamos el spinner con las cuentas del cliente
         val mbo: MiBancoOperacional? = MiBancoOperacional.getInstance(this)
         val cliente = intent.getSerializableExtra("Cliente") as Cliente
         val accountList: List<Cuenta> = mbo?.getCuentas(cliente) as List<Cuenta>
@@ -33,9 +42,33 @@ class MovementsActivity : AppCompatActivity() {
         adapterOwnAccount.setDropDownViewResource(R.layout.spiner_drop_item)
         spinnerOwnAccount.adapter = adapterOwnAccount
 
+        //mostramos los movimientos de la cuenta seleccionada
 
+        binding.spinnerMovements.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
 
+                var listaMovimientos: ArrayList<Movimiento> = mbo?.getMovimientos(accountList.get(position)) as ArrayList<Movimiento>
 
+                movementsAdapter = MovementsAdapter(listaMovimientos)
+                val context = binding.root.context
+                linearLayoutManager = LinearLayoutManager(context)
 
+                binding.rwMovements.apply {
+                    layoutManager = linearLayoutManager
+                    adapter = movementsAdapter
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
     }
+
+
 }
--- app/src/main/java/com/example/banco_jototo/MovementsAdapter.kt
+++ app/src/main/java/com/example/banco_jototo/MovementsAdapter.kt
@@ -1,4 +1,40 @@
 package com.example.banco_jototo
 
-class MovementsAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.banco_jototo.databinding.ItemAccountGbBinding
+import com.example.banco_jototo.pojo.Movimiento
+
+class MovementsAdapter (private val movements: ArrayList<Movimiento>):
+        RecyclerView.Adapter<MovementsAdapter.ViewHolder>(){
+
+            private lateinit var context: Context
+
+            inner class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+                val binding = ItemAccountGbBinding.bind(view)
+
+                //colocamos setListenner si hiciera falta
+
+            }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+        val view = LayoutInflater.from(context).inflate(R.layout.item_account_gb, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = movements.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var movement = movements.get(position)
+
+        with(holder){
+            binding.numAccountRV.text = movement.getDescripcion()
+            binding.balanceRV.text = movement.getImporte().toString()
+        }
+    }
+
 }
