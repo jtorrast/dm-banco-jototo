16a15356f2e85bf2fc7c6324c3c8bde570406d38
jtorrast <jordi.torras89@gmail.com>
2023-11-07 06:14:36 +0100

    LottieActivity.kt preparada

--- app/src/main/java/com/example/banco_jototo/LottieActivity.kt
+++ app/src/main/java/com/example/banco_jototo/LottieActivity.kt
@@ -1,11 +1,24 @@
 package com.example.banco_jototo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.banco_jototo.databinding.ActivityLottieBinding
+import com.example.banco_jototo.pojo.Cliente
 
 class LottieActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLottieBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lottie)
+        binding = ActivityLottieBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        val intent = Intent(this, MainActivity::class.java)
+        val cliente = intent.getSerializableExtra("Cliente") as Cliente
+        intent.putExtra("Cliente", cliente)
+        startActivity(intent)
     }
 }
