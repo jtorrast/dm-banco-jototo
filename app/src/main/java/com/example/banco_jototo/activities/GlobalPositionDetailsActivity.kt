2c8396102283bcf87445b847850be3b0b0e7bd43
jtorrast <jordi.torras89@gmail.com>
2023-11-21 07:14:43 +0100

    Inicio fragments

---
+++ app/src/main/java/com/example/banco_jototo/activities/GlobalPositionDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.banco_jototo.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.banco_jototo.R
+
+class GlobalPositionDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_global_position_details)
+    }
+}
