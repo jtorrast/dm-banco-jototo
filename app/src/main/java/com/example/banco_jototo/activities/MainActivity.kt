f7d7f447e71334bdbbab5caf15652e68d43eb3fe
jtorrast <jordi.torras89@gmail.com>
2023-12-07 13:02:12 +0100

    Funcionamiento ok desde vista portrait

--- app/src/main/java/com/example/banco_jototo/activities/MainActivity.kt
+++ app/src/main/java/com/example/banco_jototo/activities/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.banco_jototo.activities
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
@@ -11,12 +14,18 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.example.banco_jototo.R
 import com.example.banco_jototo.databinding.ActivityMainBinding
 import com.example.banco_jototo.fragments.AccountsFragment
+import com.example.banco_jototo.fragments.AccountsListener
 import com.example.banco_jototo.fragments.AccountsMovementsFragment
 import com.example.banco_jototo.fragments.MainActivityFragment
+import com.example.banco_jototo.fragments.MovementsListener
 import com.example.banco_jototo.pojo.Cliente
+import com.example.banco_jototo.pojo.Cuenta
+import com.example.banco_jototo.pojo.Movimiento
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.navigation.NavigationView
+import java.text.SimpleDateFormat
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, AccountsListener, MovementsListener {
 
     //private lateinit var binding: ActivityMainBinding
 
@@ -59,21 +68,72 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         val cliente = intent.getSerializableExtra("Cliente") as Cliente
         when(item.itemId){
-            R.id.nav_home -> supportFragmentManager.beginTransaction()
-                .replace(R.id.frgMainActivity, MainActivityFragment.newInstance(cliente)).commit()
-            R.id.nav_globalPosition-> supportFragmentManager.beginTransaction()
-                .replace(R.id.frgMainActivity, AccountsFragment.newInstance(cliente)).commit()
+            R.id.nav_home ->{
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frgMainActivity, MainActivityFragment.newInstance(cliente)).commit()
+
+            }
+            R.id.nav_globalPosition ->{
+                val frgMainAccounts = AccountsFragment.newInstance(cliente)
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frgMainActivity, frgMainAccounts).commit()
+                frgMainAccounts.setListener(this)
+            }
         }
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
 
     override fun onBackPressed() {
+        super.onBackPressed()
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START)
         } else {
             onBackPressedDispatcher.onBackPressed()
         }
+    }
+
+    override fun onCuentaSeleccionada(cuenta: Cuenta) {
+        val frgMovements = AccountsMovementsFragment.newInstance(cuenta)
+
+        Log.i("Configuración de pantalla", "Valor de screenLayout: ${resources.configuration.screenLayout}")
+
+        //el valor 268435796 equivale a la pantalla de la tablet
+        if (resources.configuration.screenLayout == 268435796){
+            Log.i("Dispositivo", "tablet")
+            frgMovements.setListener(this)
+
+            //tablet
+            supportFragmentManager.beginTransaction().
+            replace(R.id.frgMovimiento, frgMovements).commit()
+
+        }else{
+            //Pantalla movil, cambiamos actividad
+            val intent = Intent(this, GlobalPositionDetailsActivity::class.java)
+            intent.putExtra("Cuenta", cuenta)
+            startActivity(intent)
+        }
+    }
+
+    override fun onMovimientoSeleccionado(movimiento: Movimiento) {
+        Log.i("Global Position", movimiento.getDescripcion().toString())
+        val dialogView = layoutInflater.inflate(R.layout.dialog_movement, null)
+
+        val formateador = SimpleDateFormat("dd/MM/yyyy")
+
+        val textoInfo = dialogView.findViewById<TextView>(R.id.textInfo)
+        textoInfo.text = "Id: ${movimiento.getId()} \n" +
+                "Descripcion: ${movimiento.getDescripcion()} \n" +
+                "Fecha: ${formateador.format(movimiento.getFechaOperacion())}"
+
+        MaterialAlertDialogBuilder(this)
+            .setView(dialogView)
+            .setBackground(getDrawable(R.color.azul_oscuro_medio))
+            .setPositiveButton(resources.getString(R.string.accept), DialogInterface.OnClickListener { dialog, i ->
+                dialog.cancel()
+            })
+            .setCancelable(false)
+            .show()
     }
 
 
