184744e86bbc91c08d017aabf6e4a0caf967d8c0
jtorrast <jordi.torras89@gmail.com>
2023-12-07 12:26:31 +0100

    Estilo portrait navigation ok, funcionamiento basico de la actividad 1

--- app/src/main/java/com/example/banco_jototo/activities/MainActivity.kt
+++ app/src/main/java/com/example/banco_jototo/activities/MainActivity.kt
@@ -3,30 +3,77 @@ package com.example.banco_jototo.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.banco_jototo.R
 import com.example.banco_jototo.databinding.ActivityMainBinding
+import com.example.banco_jototo.fragments.AccountsFragment
+import com.example.banco_jototo.fragments.AccountsMovementsFragment
 import com.example.banco_jototo.fragments.MainActivityFragment
 import com.example.banco_jototo.pojo.Cliente
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    private lateinit var binding: ActivityMainBinding
+    //private lateinit var binding: ActivityMainBinding
+
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle =  ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
         val cliente = intent.getSerializableExtra("Cliente") as Cliente
 
-        val frgMain = MainActivityFragment.newInstance(cliente)
-        supportFragmentManager.beginTransaction()
-            .add(R.id.frgMainActivity, frgMain).commit()
+
+        if (savedInstanceState == null) {
+            val frgMain = MainActivityFragment.newInstance(cliente)
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.frgMainActivity, frgMain).commit()
+            navigationView.setCheckedItem(R.id.nav_home)
+        }
 
         /*Ahora tendremos que añadir los framents que llamemos desde el menu lateral*/
 
 
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val cliente = intent.getSerializableExtra("Cliente") as Cliente
+        when(item.itemId){
+            R.id.nav_home -> supportFragmentManager.beginTransaction()
+                .replace(R.id.frgMainActivity, MainActivityFragment.newInstance(cliente)).commit()
+            R.id.nav_globalPosition-> supportFragmentManager.beginTransaction()
+                .replace(R.id.frgMainActivity, AccountsFragment.newInstance(cliente)).commit()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 
 
