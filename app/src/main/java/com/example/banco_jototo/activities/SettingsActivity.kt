8afea2e537b1923c271816d24e2db6b15e057123
jtorrast <jordi.torras89@gmail.com>
2024-01-09 10:41:03 +0100

    Mejoras preferencias

--- app/src/main/java/com/example/banco_jototo/activities/SettingsActivity.kt
+++ app/src/main/java/com/example/banco_jototo/activities/SettingsActivity.kt
@@ -1,9 +1,14 @@
 package com.example.banco_jototo.activities
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.example.banco_jototo.R
+import java.util.Locale
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -22,6 +27,32 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            val languagePreference: ListPreference? = findPreference("language")
+
+            languagePreference?.setOnPreferenceChangeListener { _, newValue ->
+                val selectedLanguage = newValue.toString()
+                setLocale(selectedLanguage)
+                true
+            }
+            
+        }
+
+        private fun setLocale(language: String) {
+            val locale = Locale(language)
+            Locale.setDefault(locale)
+            val config = Configuration()
+            config.locale = locale
+            resources.updateConfiguration(config, resources.displayMetrics)
+
+            // Guardar la preferencia del idioma en SharedPreferences para usarla en futuras sesiones
+            val preferences: SharedPreferences =
+                requireActivity().getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+            preferences.edit().putString("language", language).apply()
+
+            // Reiniciar la actividad para aplicar los cambios de idioma
+            requireActivity().recreate()
+
         }
     }
 }
