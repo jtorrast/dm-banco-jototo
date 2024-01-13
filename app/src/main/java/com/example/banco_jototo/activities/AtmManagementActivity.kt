3144d732556344eb9840908eb247408878b10780
jtorrast <jordi.torras89@gmail.com>
2024-01-13 08:21:08 +0100

    AtmManagementActivity añadida

---
+++ app/src/main/java/com/example/banco_jototo/activities/AtmManagementActivity.kt
@@ -0,0 +1,12 @@
+package com.example.banco_jototo.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.banco_jototo.R
+
+class AtmManagementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_atm_management)
+    }
+}
